76b94e45a36bfaafc3302eabb43401cfa883edd9
Rick Wu <tcvrick@gmail.com>
2018-10-21 22:49:34 -0400

    Clean-up files for updated .gitignore.

--- Tiles/Tile.py
+++
@@ -1,9 +0,0 @@
-from abc import ABCMeta
-
-
-class Tile(metaclass=ABCMeta):
-
-    def __init__(self, *args, **kwargs):
-        self.pathable = False
-
-
--- Tiles/UnpathableTile.py
+++
@@ -1,9 +0,0 @@
-from Tiles.Tile import Tile
-
-
-class UnpathableTile(Tile):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.pathable = True
-        pass
